db3d825c3fe798fafec6bb6906062de7ec62a316
SadaamCoding <codingsadaam@gmail.com>
2025-02-12 12:45:54 +0000

    Second day practice javaScript

---
+++ Conditional/index.js
@@ -0,0 +1,72 @@
+//The if Statement
+
+let age=5;
+if(age>18){
+    console.log("waa Qaan gaar")
+}else{
+    console.log("saqiir")
+}
+
+// short Cut The if Statement
+
+console.log(age>=18 ? "adult":"not Adult")
+
+// if statement with multiple conditions
+let tem=89;
+
+if(tem>30){
+    console.log("its to hot outside")
+}else if(tem<10){
+    console.log("its cold Outside")
+}else{console.log("The temperature is just right")}
+
+// The switch Statement
+let day=""
+switch(day){
+    case"sabti":
+    console.log("today is sabti ")
+    break
+    case "axad":
+        console.log("today is axad ")
+        break
+        case "isniin":
+            console.log("today is isniin ")
+            break
+            case "talaado":
+                console.log("today is talaado ")
+                break
+                case "arbaco":
+                    console.log("today is arbaco ")
+                    break
+                    case "khamiis":
+                        console.log("today is khamiis ")
+                        break
+                        case "Jimco":
+                            console.log("today is Jimco ")
+
+                            default:
+                                console.log("Today is some other day.");
+            
+}
+
+// For Loop
+
+// for(let i=0; i<10; i++){
+//     console.log(i++)
+// }
+
+// while loop
+
+let i=0;
+while(i<10){
+    i++
+    console.log(i)
+   
+}
+
+// Do While Loop
+let x=0;   
+do{
+    console.log(x)
+    x++
+}while(x<10);
---
+++ Operators/index.js
@@ -0,0 +1,61 @@
+// Part (2): Operators and Expressions in JavaScript
+
+// addition
+
+let num1=20;
+let num2=30
+let sum=num1+num2;
+console.log(sum)
+
+// subtraction
+
+let n1=100;
+let n2=70;
+let subtraction=n1-n2
+console.log(subtraction)
+
+// multiplication
+
+let y=20;
+let x=2;
+let multiplication=y*x;
+console.log(multiplication)
+
+
+// Comparison Operators and Logical Operators
+
+let age =18;
+let avg=78;
+// if(age>=18 && avg>=50){
+//     console.log("Welcome Just university")
+// }else{console.log("Not Allowed ")}
+let result= age>=18 && avg>=50 ? "welcome" : "Not You"
+console.log(result)
+
+console.log(x==y)
+console.log(x!=y)
+console.log(x>y)
+console.log(x<y)
+
+let firstStrictEquality = "Hello";
+let secondStrictEquality = "World";
+console.log(firstStrictEquality === secondStrictEquality); 
+console.log(typeof(firstStrictEquality))
+
+// String Operators
+let name='sadaam'
+let sname='dahir'
+console.log(name +""+sname)
+
+// // Increment Operator (++) example
+
+let number=20;
+number++;
+console.log(number)
+
+
+// // Decrement Operator (--) example
+
+let number1=20;
+number1--;
+console.log(number1)
