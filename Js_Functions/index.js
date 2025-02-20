936db1009ce242b16221197d7bc5f1518a8e0b91
SadaamCoding <codingsadaam@gmail.com>
2025-02-20 19:29:37 +0000

    i make practice how to use different funtion javaScript

---
+++ Js_Functions/index.js
@@ -0,0 +1,54 @@
+// Example 1: Basic Function Declaration
+
+// let name='sadaam'
+// function Printhello(){
+//     console.log(`Hello ${name}`)
+// }
+// Printhello()
+
+// function sums(n1,n2){
+//     return n1+n2
+// }
+//  console.log(sums(10,30))
+
+
+// Example 4: Function with Rest Parameters
+// function restpar(...numbers){
+// return numbers
+// }
+// console.log(restpar(1,2,3,4,5,6))
+
+//  Example 1: Function Expression
+
+// const sumnumbers=function(n1,n2){
+//     return n1+n2;
+// }
+
+// console.log(sumnumbers(10,20))
+
+let magac='jimcaale'
+// const printname=function(){
+//    console.log(`Hello Mr ${magac}`)
+// }
+// printname()
+
+
+// Example 1: Arrow Function without Parameters
+
+// const name=()=>{
+//     console.log(`Hello ${magac}`)
+// }
+// name()
+
+// const sum=(num1,num2)=> num1+num2;
+// console.log(sum(100,200))
+
+// Example: Higher-Order Function
+// const sumnumbers=(a,b,operation)=> operation(a,b);
+
+// const result=sumnumbers(10,30,(a,b)=> a+b)
+// console.log(result)
+
+// const subtraction=(a,b,operation)=> operation(a,b);
+// const result=subtraction(900,300,(a,b)=> a-b)
+// console.log(result)
